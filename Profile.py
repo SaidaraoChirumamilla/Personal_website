0cee0f2c351dc4b3366c2291654bac2b5b30ca7f
SaidaraoChirumamilla <csaidarao1@student.gsu.edu>
2021-08-07 21:10:34 -0400

    Signup validation added

--- Profile.py
+++ Profile.py
@@ -1,90 +1,49 @@
 from flask import Flask, request
 from flask import render_template
-from flask.helpers import url_for
+from Forms import SignupForm
+from flask_bootstrap import Bootstrap
+from formnew import RegistrationForm
+from flask.helpers import flash, url_for
 from flask.wrappers import Request
-from signup import Signup
-import psycopg2
-from flask_wtf import FlaskForm
-# from flask_bootstrap import bootstrap
-# from flask_sqlalchemy import SQLAlchemy
+# from signup import Signup
 from werkzeug.utils import redirect
 
 
 app = Flask(__name__)
+app.secret_key = 'asrtarstaursdlarsn'
+Bootstrap(app)
 
-@app.route('/')
+@app.route('/home')
 def index():
     return render_template('home1.html')
 
+
+
+
 @app.route('/signup',methods=["GET", "POST"])
 def signup():
-    if request.method == 'POST' :
 
-        # print(request.form)
+    signupform = SignupForm()
 
-        x = Signup(request.form)
+    if request.method == "POST" :
 
-        print("x =",x)
+        print(request.form.getlist('grade'))
 
-        # username = request.form['username']
-        # password = request.form['password']
         
-        # email = request.form['email']
-
-        # print(email,password,username)
-
-        return render_template('Signup.html')
-    return render_template('Signup.html')
 
 
-@app.route('/home', methods=["GET", "POST"])
-def home():
-    ButtonPressed = "success"
-    if request.method == "POST":
-        return render_template("home.html", ButtonPressed )
-    return render_template('home.html')
+        if signupform.validate_on_submit():
+            # print(request.form)
 
-@app.route('/aboutme')
-def aboutme():
-    return render_template('AboutMe.html')
+            flash(f'Account created for {signupform.username.data}!', 'success')
+            return render_template('home1.html')
 
-@app.route('/courses')
-def Courses():
-    return render_template('Courses.html')
+    print(signupform.errors)
+    return render_template('signup.html', signupform = signupform)
 
 
-@app.route("/projects", methods=["GET", "POST"])
-def projects():
-    if request.method == 'POST' :
-
-        Email = request.form['email']
-        firstName = request.form['fname']
-        secondname = request.form['sname']
-        msg = request.form['msg']
-
-        print(Email,firstName,secondname,msg)
-
-        return render_template('Projects.html',firstName  = firstName, secondname = secondname, Email = Email, msg = msg)
-
-        # redirect('/projects', firstName  = firstName, secondname = secondname, Email = Email, msg = msg)
 
 
-    else :
-        return render_template('Projects.html')
-
-
-@app.route('/experince')
-def experince():
-    return render_template('Experince.html')
-
-@app.route('/techicalskills')
-def techicalskills():
-    return render_template('TechnicalSkills.html')
-
-
-@app.route('/Contact')
-def Contact():
-    return render_template('AboutMe.html')
 
 if __name__ == "__main__":
     app.run( debug=True, port=5000)
--- signup.py
+++ signup.py
@@ -1,33 +1,41 @@
-import psycopg2
-from wtforms import Form, BooleanField, StringField, PasswordField, validators
+# import psycopg2
+# # from wtforms import validators,StringField
 
 
 
-def Signup(form):
-    connection = psycopg2.connect(user="teja",
-                                  password="Saidarao3!",
-                                  host="127.0.0.1",
-                                  port="5432",
-                                  database="iamstem")
+# def Signup(form):
+#     connection = psycopg2.connect(user="teja",
+#                                   password="Saidarao3!",
+#                                   host="127.0.0.1",
+#                                   port="5432",
+#                                   database="iamstem")
 
 
-   # Create a cursor to perform database operations
-    cursor = connection.cursor()
+#    # Create a cursor to perform database operations
+#     cursor = connection.cursor()
 
-    
-    fname = form['firstName']
-    sname = form['lastName']
-    password = form['password']
-    email = form['email']
-    roles = form['roles']
+#     if len(form['firstName'])>4 and len(form['lastName'])>4 and validators.email(form['email']):
 
-    print(type(form['firstName']))
+#         fname = form['firstName']
+#         sname = form['lastName']
+#         password = form['password']
+#         email = form['email']
+#         roles = form['roles']
+#         user_insert_query = """INSERT INTO users (firstname, lastname, email,pass,rol,phone,grade) \
+#               VALUES (%s,%s,%s,%s,%s,%s,%s)"""
+#         cursor.execute(user_insert_query,(fname,sname,email,password,roles,fname,fname))
+#         connection.commit()
+#     else:
+#         return 0
 
-    user_insert_query = """INSERT INTO users (firstname, lastname, email,pass,rol,phone,grade) \
-    VALUES (%s,%s,%s,%s,%s,%s,%s)"""
+# # class SignupForm():
 
-
-    cursor.execute(user_insert_query,(fname,sname,email,password,roles,fname,fname))
-    connection.commit()
-
+# #     username = StringField('Username',
+# #                            validators=[DataRequired(), Length(min=2, max=20)])
+# #     email = StringField('Email',
+# #                         validators=[DataRequired(), Email()])
+# #     password = PasswordField('Password', validators=[DataRequired()])
+# #     confirm_password = PasswordField('Confirm Password',
+# #                                      validators=[DataRequired(), EqualTo('password')])
+# #     submit = SubmitField('Sign Up')
 
